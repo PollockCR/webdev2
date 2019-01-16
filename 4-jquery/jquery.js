4156c603cf8406ef4b40e92d9fb77e0bbc5e09d2
Catherine Pollock <pollockcr@gmail.com>
2019-01-16 09:39:11 -1000

    Finish 4-draggable and resizeable

--- 4-jquery/jquery.js
+++ 4-jquery/jquery.js
@@ -1,1 +1,9 @@
-$("#draggable").draggable();
+$("#draggable").draggable({ containment: "parent" });
+
+$("#resizeable").resizable({
+    resize: function( event, ui ) {
+        if($("#resizeable").width() > 600){
+            alert("big enough");
+        }
+    }
+});
