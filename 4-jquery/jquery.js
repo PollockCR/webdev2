a67aa2f7f85450722160564de9eb9d0afd4a8b6d
Catherine Pollock <pollockcr@gmail.com>
2019-01-17 10:28:38 -1000

    Code player menu completed

--- 4-jquery/jquery.js
+++ 4-jquery/jquery.js
@@ -1,2 +1,46 @@
-$("#accordion").accordion();
-$("#sortable").sortable();
+$(".btn").click(function() {
+    if ($(this).hasClass("active")){
+        $(this).removeClass("active");
+        $(this).addClass("inactive");
+        hidePanel($(this).attr("id"));
+        
+    } else {
+        $(this).addClass("active");
+        $(this).removeClass("inactive");
+        showPanel($(this).attr("id"));
+    }
+});
+
+function showPanel(id){
+    switch (id) {
+        case "htmlButton":
+            $("#html").show();
+            break;
+        case "cssButton":
+            $("#css").show();
+            break;
+        case "javascriptButton":
+            $("#javascript").show();
+            break;
+        case "outputButton":
+            $("#output").show();
+            break;
+    }
+}
+
+function hidePanel(id){
+    switch (id) {
+        case "htmlButton":
+            $("#html").hide();
+            break;
+        case "cssButton":
+            $("#css").hide();
+            break;
+        case "javascriptButton":
+            $("#javascript").hide();
+            break;
+        case "outputButton":
+            $("#output").hide();
+            break;
+    }
+}
