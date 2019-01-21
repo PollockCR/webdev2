132983b303af017840dc12785d2f0243416170f0
Catherine Pollock <pollockcr@gmail.com>
2019-01-21 13:58:25 -1000

    Finish 6-variables

--- 6-php/index.php
+++ 6-php/index.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "Hello world!";
+$myNameIs = "My name is";
+
+$name = "Cat";
+
+echo $myNameIs." ".$name;
+
+$myNumber = 324325;
+
+$myNumber = $myNumber * 3 / 5 + 11;
+
+echo $myNumber;
+
+$myBool = true;
+
+echo $myBool;
+
+$variableName = "name";
+
+echo $$variableName;
     
 ?>
