753ffce599f692a1d5697fea1fdff68f98723f63
Catherine Pollock <pollockcr@gmail.com>
2019-01-24 14:16:01 -1000

    Finish 6-weather scraper

--- 6-php/index.php
+++ 6-php/index.php
@@ -1,8 +1,31 @@
 <?php 
 
-    include("sendForm.php");
+$weather = "";
 
-    // echo file_get_contents("https://exampledomain.com");
+if($_GET && array_key_exists("city", $_GET)){
+
+    $city = str_replace(" ", "-", $_GET["city"]);
+
+    $weather = file_get_contents("https://www.weather-forecast.com/locations/$city/forecasts/latest");
+
+    $city = ucwords($_GET["city"]);
+
+    if(!$weather){
+
+
+        $weather = '<div class="alert alert-danger" role="alert">The city '.$city.' could not be found. Please try again.</div>';
+
+    } else {
+
+        $weather = strstr($weather,'<span class="phrase">');
+
+        $weather = strstr($weather, '</p>', true);
+
+        $weather = '<div class="alert alert-secondary" role="alert"><strong>'.$city."</strong> weather today: ".$weather.'</div>';
+
+    }
+
+}
 
 ?>
 
@@ -10,46 +33,65 @@
 
     <head>
 
-        <title>Contact Us</title>
+        <title>Weather Scraper</title>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+
     </head>
 
     <body>
 
-        <div class="container">
+        <div class="container-flex">
 
-            <h1>Get in touch!</h1>
+            <div class="d-flex justify-content-center">
 
-            <div id="success"><?php echo $success ?></div>
+                <h1 class="text-light">What's the Weather?</h1>
 
-            <div id="error"><?php echo $error ?></div>
+            </div>
 
-            <form id="target" method="post">
+            <div class="d-flex justify-content-center">
 
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" required>
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
+                <p class="text-light">Enter the name of a city.</p>
 
-                <div class="form-group">
-                    <label for="subject">Subject</label>
-                    <input type="text" class="form-control" id="subject" name="subject" required>
-                </div>
+            </div>
 
-                <div class="form-group">
-                    <label for="content">How can we help you?</label>
-                    <textarea class="form-control" name="content" id="content" rows="3" required></textarea>
-                </div>
+            <div class="d-flex justify-content-center mt-3">
 
-                <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+                <form class="form-inline">
 
-            </form>
+                    <label class="sr-only" for="city">City</label>
+                    <input type="text" class="form-control mb-2 mr-sm-2" name="city" id="city" placeholder="e.g. Paris">
+
+                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+
+                </form>
+
+            </div>
+
+            <div class="col-sm-8 mx-auto">
+
+                <?php 
+
+                echo $weather;
+
+                ?>
+
+            </div>
+
+            <div id="credits">
+                
+                <a href="https://unsplash.com/@jhonkasalo?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Joakim Honkasalo" class="credit"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Joakim Honkasalo</span></a>   
+
+                <a href="https://www.weather-forecast.com/" target="_blank" rel="noopener noreferrer" title="Weather Forecast" class="credit"><span style="display:inline-block;padding:2px 3px"><i class="fas fa-sun"></i> Weather Forecast</span></a>                
+
+            </div>
 
         </div>
 
