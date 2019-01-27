491905eab4fea5d5741bd131de9009ea17009e2e
Catherine Pollock <pollockcr@gmail.com>
2019-01-26 15:43:03 -1000

    Finish 7-cookies

--- 7-mysql/index.php
+++ 7-mysql/index.php
@@ -1,78 +1,10 @@
 <?php 
 
-session_start();
+setcookie("customerId", "1234", time() + 60 * 60 * 24); // 1 day
 
-include("secrets.php");
+//setcookie("customerId", "", time() - 60 * 60); // unset/delete cookie
 
-$username = USERNAME;
-
-$password = PASSWORD;
-
-$link = mysqli_connect("shareddb-m.hosting.stackcp.net", $username, $password, $username);
-
-if (mysqli_connect_error()){
-
-    die ("Error: The database could not be accessed.");
-
-}
-
-if ($_POST && array_key_exists("email", $_POST) && array_key_exists("name", $_POST) && array_key_exists("password", $_POST)){
-
-    $query = "SELECT id FROM `users` WHERE `email` = '".mysqli_real_escape_string($link, $_POST["email"])."'";
-
-    // Use mysqli_real_escape_string to prevent SQL injection
-
-    $result = mysqli_query($link, $query);
-
-    if ($result->num_rows != 0){
-
-        echo "Insert error. Email already exists in database.<br>";
-
-
-    } else {
-
-        $query = "INSERT INTO `users` (`id`, `email`, `password`, `name`) VALUES (NULL, '".mysqli_real_escape_string($link, $_POST["email"])."', '".mysqli_real_escape_string($link, $_POST["password"])."', '".mysqli_real_escape_string($link, $_POST["name"])."')";
-
-        mysqli_query($link, $query);
-
-        $_SESSION["email"] = $_POST["email"];
-
-        header("Location: session.php");
-
-    }
-
-} else if ($_POST && array_key_exists("email", $_POST) && array_key_exists("password", $_POST)){
-
-    $query = "SELECT `password` FROM `users` WHERE `email` = '".mysqli_real_escape_string($link, $_POST["email"])."'";
-
-    // Use mysqli_real_escape_string to prevent SQL injection
-
-    $result = mysqli_query($link, $query);
-
-    if ($result->num_rows != 0){
-
-        $row = mysqli_fetch_row($result);
-
-        if($row["0"] == $_POST["password"]){
-
-            $_SESSION["email"] = $_POST["email"];
-
-            header("Location: session.php");
-
-        } else {
-
-            echo "Incorrect email or password";
-
-        }
-
-
-    } else {
-
-        echo "Incorrect email or password";
-
-    }
-
-}
+echo $_COOKIE["customerId"];
 
 ?>
 
@@ -83,32 +15,6 @@ if ($_POST && array_key_exists("email", $_POST) && array_key_exists("name", $_PO
     </head>
 
     <body>
-
-        <p>Sign up:</p>
-
-        <form method="post">
-
-            <input type="text" name="name" placeholder="Name" required>
-
-            <input type="email" name="email" placeholder="Email" required>
-
-            <input type="password" name="password" placeholder="Password" required>
-
-            <input type="submit" value="Sign Up">
-
-        </form>
-        
-        <p>Sign in:</p>
-
-        <form method="post">
-
-            <input type="email" name="email" placeholder="Email" required>
-
-            <input type="password" name="password" placeholder="Password" required>
-
-            <input type="submit" value="Sign In">
-
-        </form>
 
     </body>
 
