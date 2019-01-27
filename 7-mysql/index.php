2abeb2a2f83a8108d945bfae50fecd89756e5a16
Catherine Pollock <pollockcr@gmail.com>
2019-01-27 13:56:28 -1000

    Finish 7-securing passwords with passwords_hash()

--- 7-mysql/index.php
+++ 7-mysql/index.php
@@ -1,8 +1,21 @@
-<?php 
+<?php
 
-$row['id'] = 74;
-
-echo md5(md5($row['id'])."password");
+// Generate a hash of the password "mypassword"
+$hash = password_hash("mypassword", PASSWORD_DEFAULT);
+ 
+// Echoing it out, so we can see it:
+echo $hash;
+ 
+// Some line breaks for a cleaner output:
+echo "<br><br>";
+ 
+// Using password_verify() to check if "mypassword" matches the hash.
+// Try changing "mypassword" below to something else and then refresh the page.
+if (password_verify('mypassword', $hash)) {
+    echo 'Password is valid!';
+} else {
+    echo 'Invalid password.';
+}
 
 ?>
 
