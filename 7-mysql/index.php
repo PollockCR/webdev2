2d8ac5d0920d42ee7ffeea77d975aae74f8c593b
Catherine Pollock <pollockcr@gmail.com>
2019-01-27 15:36:28 -1000

    Finish 7-secret diary pt 1-login system

--- 7-mysql/index.php
+++ 7-mysql/index.php
@@ -1,20 +1,130 @@
-<?php
+<?php 
 
-// Generate a hash of the password "mypassword"
-$hash = password_hash("mypassword", PASSWORD_DEFAULT);
- 
-// Echoing it out, so we can see it:
-echo $hash;
- 
-// Some line breaks for a cleaner output:
-echo "<br><br>";
- 
-// Using password_verify() to check if "mypassword" matches the hash.
-// Try changing "mypassword" below to something else and then refresh the page.
-if (password_verify('mypassword', $hash)) {
-    echo 'Password is valid!';
-} else {
-    echo 'Invalid password.';
+session_start();
+
+include("secrets.php");
+
+$username = USERNAME;
+
+$password = PASSWORD;
+
+$link = mysqli_connect("shareddb-m.hosting.stackcp.net", $username, $password, $username);
+
+if (mysqli_connect_error()){
+
+    die ("Error: The database could not be accessed.");
+
+}
+
+if ($_POST && array_key_exists("submit", $_POST)){
+
+    // sign up
+
+    if ($_POST["submit"] == "Sign Up" && array_key_exists("email", $_POST) && array_key_exists("password", $_POST)){
+
+        $query = "SELECT id FROM `users` WHERE `email` = '".mysqli_real_escape_string($link, $_POST["email"])."'";
+
+        $result = mysqli_query($link, $query);
+
+        if ($result->num_rows != 0){
+
+            echo "Sign up error: Email already exists in database.<br>";
+
+
+        } else {
+
+            $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+            $query = "INSERT INTO `users` (`id`, `email`, `password`, `name`) VALUES (NULL, '".mysqli_real_escape_string($link, $_POST["email"])."', '".$password."', '')";
+
+            mysqli_query($link, $query);
+
+            $_SESSION["email"] = $_POST["email"];
+
+            if(array_key_exists("remember-sign-up", $_POST) && $_POST["remember-sign-up"]){
+
+                setcookie("email", $_POST["email"], time() + 60 * 60 * 24 * 1);
+
+                setcookie("password", $password, time() + 60 * 60 * 24 * 1);
+
+            }
+
+            header("Location: session.php");
+
+        }
+
+    } else if ($_POST["submit"] == "Sign In" && array_key_exists("email", $_POST) && array_key_exists("password", $_POST)){
+
+        // sign in 
+
+        $query = "SELECT `password` FROM `users` WHERE `email` = '".mysqli_real_escape_string($link, $_POST["email"])."'";
+
+        $result = mysqli_query($link, $query);
+
+        if ($result->num_rows != 0){
+
+            $row = mysqli_fetch_row($result);
+
+            if(password_verify($_POST["password"], $row["0"])){
+
+                $_SESSION["email"] = $_POST["email"];
+
+                if(array_key_exists("remember-sign-in", $_POST) && $_POST["remember-sign-in"]){
+
+                    setcookie("email", $_POST["email"], time() + 60 * 60 * 24 * 1);
+
+                    setcookie("password", $row["0"], time() + 60 * 60 * 24 * 1);
+
+                }
+
+                header("Location: session.php");
+
+            } else {
+
+                echo "Incorrect email or password";
+
+            }
+
+
+        } else {
+
+            echo "Incorrect email or password";
+
+        }
+
+    }
+
+} else if ($_COOKIE && array_key_exists("email", $_COOKIE) && array_key_exists("password", $_COOKIE)){
+
+    // remember me 
+
+    $query = "SELECT `password` FROM `users` WHERE `email` = '".mysqli_real_escape_string($link, $_COOKIE["email"])."'";
+
+    $result = mysqli_query($link, $query);
+
+    if ($result->num_rows != 0){
+
+        $row = mysqli_fetch_row($result);
+
+        if ($_COOKIE["password"] == $row["0"]){
+
+            $_SESSION["email"] = $_COOKIE["email"];
+
+            header("Location: session.php");
+
+        } else {
+
+            echo "Please log in again.";
+
+        }
+
+
+    } else {
+
+        echo "Please log in again.";
+
+    }
+
 }
 
 ?>
@@ -26,6 +136,34 @@ if (password_verify('mypassword', $hash)) {
     </head>
 
     <body>
+
+        <p>Sign up:</p>
+
+        <form method="post">
+
+            <input type="email" name="email" placeholder="Email" required>
+
+            <input type="password" name="password" placeholder="Password" required>
+
+            <input type="checkbox" name="remember-sign-up">
+
+            <input type="submit" value="Sign Up" name="submit">
+
+        </form>
+
+        <p>Sign in:</p>
+
+        <form method="post">
+
+            <input type="email" name="email" placeholder="Email" required>
+
+            <input type="password" name="password" placeholder="Password" required>
+
+            <input type="checkbox" name="remember-sign-in">
+
+            <input type="submit" value="Sign In" name="submit">
+
+        </form>
 
     </body>
 
--- 7-mysql/session.php
+++ 7-mysql/session.php
@@ -2,15 +2,46 @@
 
 session_start();
 
-if($_SESSION && array_key_exists("email", $_SESSION)){
-    
-    echo "Welcome, ".$_SESSION["email"];
-    
-} else {
-    
-    header("Location: index.php");
-    
-}
+if ($_POST && array_key_exists("sign-out", $_POST) && $_POST["sign-out"] == "Sign Out"){
 
+    setcookie("email", "", time() - 60);
+
+    setcookie("password", "", time() - 60);
+
+    if ($_SESSION && array_key_exists("email", $_SESSION)){
+
+        unset($_SESSION["email"]);
+
+    }
+
+    header("Location: index.php");
+
+} else if ($_SESSION && array_key_exists("email", $_SESSION)){
+
+    echo "Welcome, ".$_SESSION["email"];
+
+} else {
+
+    header("Location: index.php");
+
+} 
 
 ?>
+
+<html>
+
+    <head>
+    </head>
+
+    <body>
+
+        <form method="post">
+
+            <input type="submit" value="Sign Out" name="sign-out">
+
+        </form>
+        
+    </body>
+
+</html>
+
