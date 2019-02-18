65325b140f761468a7102e3db0712db860df1b20
Catherine Pollock <pollockcr@gmail.com>
2019-02-17 21:59:58 -0600

    Create Twitter footer

--- 12-twitter/views/header.php
+++ 12-twitter/views/header.php
@@ -1,16 +1,18 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" class="h-100">
     <head>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
+        <!-- CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="./style.css">
 
         <title>Hello, world!</title>
     </head>
-    <body>
+    <body class="d-flex flex-column h-100">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- 12-twitter/views/home.php
+++ 12-twitter/views/home.php
@@ -1,5 +1,5 @@
-<div class="container">
-    
-    <h1>Hello, world!</h1>
-    
-</div>
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Hello world!</h1>
+  </div>
+</main>
