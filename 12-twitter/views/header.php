3c7128ee46204d418ef038598c66f1f820440fc6
Catherine Pollock <pollockcr@gmail.com>
2019-02-21 17:10:30 -0600

    Adjust Twitter navbar breakpoint

--- 12-twitter/views/header.php
+++ 12-twitter/views/header.php
@@ -13,7 +13,7 @@
         <title>Wannabe Twitter</title>
     </head>
     <body class="d-flex flex-column h-100">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
