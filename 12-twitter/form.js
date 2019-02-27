d9a74b2d253e9fb722a72de8287cea36ab2cfe59
Catherine Pollock <pollockcr@gmail.com>
2019-02-26 23:10:04 -0600

    Simplify Twitter JS form navigation

--- 12-twitter/form.js
+++ 12-twitter/form.js
@@ -7,7 +7,7 @@ $("#loginForm").submit(function (event) {
         data: "email=" + $("#loginEmail").val() + "&password=" + $("#loginPassword").val(),
         success: function(result) {
             if(result == "1"){
-                window.location.assign("http://www.catherinepollock.com/12-twitter/")
+                //window.location.assign("http://www.catherinepollock.com/12-twitter/")
             } else {
                 $("#loginAlert").html(result).show()
                 event.preventDefault()
@@ -27,7 +27,7 @@ $("#signupForm").submit(function (event) {
         data: "email=" + $("#signupEmail").val() + "&password=" + $("#signupPassword").val(),
         success: function(result) {
             if(result == "1"){
-                window.location.assign("http://www.catherinepollock.com/12-twitter/")
+                //window.location.assign("http://www.catherinepollock.com/12-twitter/")
             } else {
                 $("#signupAlert").html(result).show()
                 event.preventDefault()
